c4139a6469ed8475a6b8020ccbba783b894b62d7
LeonAndrade <35314330+LeonAndrade@users.noreply.github.com>
2023-02-12 16:55:09 -0300

    dockerizing application

--- mateus-silva-jstack/my-contacts/backend/src/database/index.js
+++ mateus-silva-jstack/my-contacts/backend/src/database/index.js
@@ -3,9 +3,9 @@ const { Client } = require('pg');
 const client = new Client({
   host: 'localhost',
   port: 5432,
-  user: 'root',
-  password: 'root',
-  database: 'mycontacts'
+  user: 'mycontacts_user',
+  password: 'mycontacts_pw',
+  database: 'mycontacts_db'
 });
 
 client.connect();
